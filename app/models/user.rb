116ae688dd9f12f5fb75e4dc179707d0d8d2f666
Ilan Attia <attia.ilan@gmail.com>
2024-11-18 17:47:40 +0100

    Changes to DB and controllers created

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :bookings
+  validates :email, uniqueness: true
+  validates :email, :password, :last_name, :first_name, presence: true
+  validates :passord, length: { minimum: 8 }
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
 end
