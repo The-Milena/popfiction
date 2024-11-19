d9fd7954a0343aa241e30fcf5fd2c9d49412c393
Ilan Attia <attia.ilan@gmail.com>
2024-11-19 16:21:00 +0100

    view show

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,8 +2,7 @@ class Booking < ApplicationRecord
   belongs_to :place
   belongs_to :user
   validates :date_end, :date_start, presence: true
-  validates :date_end, comparison: { greater_than: :date_start, Date.today }
+  validates :date_end, comparison: { greater_than: [:date_start, Date.today] }
   validates :date_start, comparison: { greater_than: Date.today }
   validates :nb_of_guests, presence: true, numericality: { only_integer: true, greater_than: 0 }
-
 end
