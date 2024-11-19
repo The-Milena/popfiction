0467c4a7f0fa8b64bd72d8679ccbff743cbba22e
Milena Arms <183767485+The-Milena@users.noreply.github.com>
2024-11-19 13:01:31 +0100

    took away pciture from places

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,11 +2,10 @@ class Place < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_many_attached :photos
-  validates :address, :beds, :price, :picture, presence: true
+  validates :address, :beds, :price, presence: true
   validates :address, uniqueness: true
   validates :beds, numericality: { greater_than: 0 }
   validates :beds, numericality: { only_integer: true }
   validates :description, length: { minimum: 120 }
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
-  validates :picture, presence: true
 end
