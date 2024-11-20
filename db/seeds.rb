c5949031db0070f582de9633f6437aa72d946214
Nina Ratsimihah <nratsimi@gmail.com>
2024-11-20 12:55:45 +0100

    destroy booking

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,7 +30,7 @@
 #  t.index ["email"], name: "index_users_on_email", unique: true
 #  t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 #end
-
+Booking.destroy_all
 Place.destroy_all
 User.destroy_all
 
