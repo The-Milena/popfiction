9724b285843c9036d18467562af7ff9b9d17027b
Ilan Attia <attia.ilan@gmail.com>
2024-11-20 12:53:59 +0100

    create new place + btn destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # Relations
   has_many :bookings
+  has_many :places, dependent: :destroy # Ajoute la relation avec Place
+
+  # Validations
   validates :email, uniqueness: true
   validates :email, :password, :last_name, :first_name, presence: true
   validates :password, length: { minimum: 8 }
